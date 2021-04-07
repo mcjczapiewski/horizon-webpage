54846a645bb946f31ba164ffa56ec2c3be48a40c
Maciej Czapiewski <mcjczapiewski@gmail.com>
2021-04-07 20:34:23 +0200

    Change GSV, add borders and shadows

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ function Home() {
                 </span>
                 <span className="google-span">
                     <span className="google-text">
-                        <h3>Wspieramy rozwój Google Maps</h3>
+                        <h3>Rozwijamy Google Street View</h3>
                         <img
                             className="maps-icon"
                             src="../../maps-icon.png"
@@ -31,14 +31,15 @@ function Home() {
                         />
                     </span>
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!4v1617136607651!6m8!1m7!1sCAoSLEFGMVFpcE9WSmpMRjVqMWxSNFRadkMwbll0Y29TWmM1SDdXY0pYVFdqbEtY!2m2!1d52.32080097!2d20.97492814!3f314.08!4f-66.8!5f0.8635291576552315"
-                        width="600"
-                        height="450"
+                        src="https://www.google.com/maps/embed?pb=!4v1617820045342!6m8!1m7!1sCAoSLEFGMVFpcE0xSXZpbTFGak1YdFBsUWZOWFE3c2lWdWFEcWwtVlhLNVJ6eGZa!2m2!1d52.2678734!2d21.0618451!3f331.8381529629467!4f19.556747678851536!5f0.9028122286144149"
+                        width="500"
+                        height="500"
                         style={{ border: "0" }}
                         allowfullscreen=""
                         loading="lazy"
                         title="GoogleMaps"
                         id="google-iframe"
+                        rel="noopener noreferrer"
                     ></iframe>
                 </span>
             </div>
@@ -50,6 +51,20 @@ function Home() {
                 <br />
                 Zapraszamy do kontaktu :-)
             </h4>
+            <h2>
+                Zobacz gdzie już byliśmy!
+                <br />
+                Szukaj na mapie&nbsp;
+                <img className="icon-horizon" src="./icon.png" alt="" />
+            </h2>
+            <iframe
+                src="https://www.google.com/maps/d/u/1/embed?mid=1DMe7n9Y17vkRPAhOGwqRahjHLmZwJsx4"
+                width="500px"
+                height="500px"
+                title="we-were-here"
+                id="google-we-were-here"
+                rel="noopener noreferrer"
+            ></iframe>
             <div className="changing">
                 <h2>Zmieniamy się dla Ciebie</h2>
                 <div className="dot-windmill"></div>
