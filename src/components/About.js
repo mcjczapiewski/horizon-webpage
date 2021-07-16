5bf205e33d5578474b456939d654388fea41737c
Maciej Czapiewski <mcjczapiewski@gmail.com>
2021-07-16 18:28:28 +0200

    Add About us text

--- src/components/About.js
+++ src/components/About.js
@@ -4,19 +4,54 @@ import mapsIcon from "../assets/images/maps-icon.svg";
 const About = () => {
     return (
         <div className="About">
-            {/* <div className="column-one">
+            <div className="column-one">
                 <div className="about-text">
+                    Jesteśmy pasjonatami nowoczesnych rozwiązań i fotografii.
+                    <br />
+                    <br />
                     Zaczynaliśmy od wykonywania ujęć z drona, a teraz schodzimy
-                    na ziemię i udostępniamy zainteresowanym możliwość zdalnego
-                    wglądu do wnętrz pomieszczeń. Zwłaszcza teraz, kiedy mamy
-                    ograniczoną możliwość przemieszczania się, chętniej
-                    sprawdzamy, jak wygląda lokal, zanim odwiedzimy go
-                    osobiście. Z własnego doświadczenia wiemy, że klient lubi
-                    wiedzieć, co go czeka, dotyczy to wyglądu i rozmieszczenia
-                    pomieszczeń, do których się wybiera, ale też ogólnej
-                    przestrzeni, po której będzie się poruszał.
+                    na ziemię i udostępniamy wirtualną rzeczywistość.
+                    <br />
+                    <br />
+                    Męska precyzja i znajomość najnowszych trendów
+                    informatycznych oraz kobiece poczucie estetyki i wrażliwość
+                    na detale pomogły nam stworzyć duet (prawie) idealny.
+                    <br />
+                    <br />
+                    Do każdego miejsca podchodzimy kreatywnie i nieszablonowo, a
+                    podczas realizacji projektu, dopracowujemy szczegóły do
+                    momentu, aż w spełni oczekiwania klienta.
+                    <br />
+                    <br />
+                    Realizujemy zlecenie na terenie całej Polski, a dojazd jest
+                    całkowicie bezpłatny.
+                    <br />
+                    <br />
+                    Skontaktuj się z nami, a pomożemy Ci zrealizować:
+                    <ul>
+                        <li>Panoramiczne zdjęcia 360°</li>
+                        <li>Wirtualny spacer</li>
+                        <li>Wizytówkę Google</li>
+                    </ul>
+                    Robimy zdjęcia dla:
+                    <ul>
+                        <li>Hoteli</li>
+                        <li>Restauracji</li>
+                        <li>Muzeów</li>
+                        <li>Atrakcji turystycznych</li>
+                        <li>Salonów kosmetycznych i fryzjerskich</li>
+                        <li>Gabinetów lekarskich i dentystycznych</li>
+                        <li>Deweloperów</li>
+                        <li>Sklepów</li>
+                        <li>Salonów Fitness</li>
+                        oraz wielu innych branż.
+                    </ul>
+                    <div className="tell-us">
+                        Powiedz nam czego potrzebujesz i zapytaj o bezpłatną
+                        wycenę!
+                    </div>
                 </div>
-            </div> */}
+            </div>
             <div className="column-two">
                 <span className="google-span">
                     <span className="google-text">
