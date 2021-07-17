91e9bb41bff0f90fb9276a6feeffb92de060a9d4
Maciej Czapiewski <mcjczapiewski@gmail.com>
2021-07-17 17:59:19 +0200

    Change bottom arrow visibility

--- src/components/CopyrightBar.js
+++ src/components/CopyrightBar.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { ExternalLinks } from "./ExternalLinks";
 
 const CopyrightBar = ({ scrollToTop }) => {
     const executeScroll = () => {
         scrollToTop.current.scrollTo(0, 0);
     };
+    const location = useLocation();
+
+    useEffect(() => {
+        const height =
+            document.getElementsByClassName("inner-main")[0].clientHeight;
+        const pageHeight = window.innerHeight;
+        if (height > pageHeight - 80 - 50 || location.pathname == "/gallery") {
+            document
+                .getElementsByClassName("fa-arrow-alt-circle-up")[0]
+                .classList.add("visible");
+        }
+        return () => {
+            document
+                .getElementsByClassName("fa-arrow-alt-circle-up")[0]
+                .classList.remove("visible");
+        };
+    }, [location.pathname]);
 
     return (
         <div className="copyright-bar">
