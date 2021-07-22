dd3d652a56f43e9e81cba9f7f3fe8707d5075864
Maciej Czapiewski <mcjczapiewski@gmail.com>
2021-07-22 19:54:39 +0200

    Move fetch to its own component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
+import { FetchComponent } from "./FetchComponent";
 
 const Gallery = ({ panoScroll, setPanoScroll, vtScroll, setVtScroll }) => {
     const panoRef = useRef(null);
     const vtRef = useRef(null);
     const [galleryItems, setGalleryItems] = useState([]);
     const [renderGallery, setRenderGallery] = useState(false);
+    const apiLink = "http://localhost:8010/proxy/vt/GalleryItems.json";
+    // const apiLink = "https://horizon17.pl/vt/GalleryItems.json";
 
     useEffect(() => {
         if (panoScroll) {
@@ -21,36 +24,12 @@ const Gallery = ({ panoScroll, setPanoScroll, vtScroll, setVtScroll }) => {
     });
 
     useEffect(() => {
-        let isMounted = true;
-        if (isMounted) {
-            getGalleryItems();
-        }
         return () => {
-            isMounted = false;
             setRenderGallery(false);
         };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    async function getGalleryItems() {
-        const response = await fetch(
-            // "https://horizon17.pl/vt/GalleryItems.json",
-            "http://localhost:8010/proxy/vt/GalleryItems.json",
-            {
-                method: "get",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-            }
-        );
-        await response
-            .json()
-            .then((jsonData) => {
-                setGalleryItems(jsonData);
-            })
-            .then(setRenderGallery(true));
-    }
+    FetchComponent(apiLink, setGalleryItems, setRenderGallery);
 
     if (!renderGallery) {
         return <div className="Gallery">Loading...</div>;
