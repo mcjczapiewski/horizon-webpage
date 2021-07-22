03caad905840608bcc525e6d38d9af78bd7399ad
Maciej Czapiewski <mcjczapiewski@gmail.com>
2021-07-22 19:19:15 +0200

    Fix bottom arrow position

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,6 +4,7 @@ const Gallery = ({ panoScroll, setPanoScroll, vtScroll, setVtScroll }) => {
     const panoRef = useRef(null);
     const vtRef = useRef(null);
     const [galleryItems, setGalleryItems] = useState([]);
+    const [renderGallery, setRenderGallery] = useState(false);
 
     useEffect(() => {
         if (panoScroll) {
@@ -26,14 +27,15 @@ const Gallery = ({ panoScroll, setPanoScroll, vtScroll, setVtScroll }) => {
         }
         return () => {
             isMounted = false;
+            setRenderGallery(false);
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     async function getGalleryItems() {
         const response = await fetch(
-            "https://horizon17.pl/vt/GalleryItems.json",
-            // "http://localhost:8010/proxy/vt/GalleryItems.json",
+            // "https://horizon17.pl/vt/GalleryItems.json",
+            "http://localhost:8010/proxy/vt/GalleryItems.json",
             {
                 method: "get",
                 headers: {
@@ -42,32 +44,54 @@ const Gallery = ({ panoScroll, setPanoScroll, vtScroll, setVtScroll }) => {
                 },
             }
         );
-        await response.json().then((jsonData) => setGalleryItems(jsonData));
+        await response
+            .json()
+            .then((jsonData) => {
+                setGalleryItems(jsonData);
+            })
+            .then(setRenderGallery(true));
     }
 
-    return (
-        <div className="Gallery">
-            <br />
-            <h2 ref={vtRef}>Wirtualne spacery</h2>
-            <div className="tours-gallery">
-                {galleryItems.map(({ title, url, cName, imgLink, type }) => {
-                    if (type === "VT") {
-                        return galleryRender(imgLink, cName, title, url);
-                    }
-                    return false;
-                })}
+    if (!renderGallery) {
+        return <div className="Gallery">Loading...</div>;
+    } else {
+        return (
+            <div className="Gallery">
+                <h2 ref={vtRef}>Wirtualne spacery</h2>
+                <div className="tours-gallery">
+                    {galleryItems.map(
+                        ({ title, url, cName, imgLink, type }) => {
+                            if (type === "VT") {
+                                return galleryRender(
+                                    imgLink,
+                                    cName,
+                                    title,
+                                    url
+                                );
+                            }
+                            return false;
+                        }
+                    )}
+                </div>
+                <h2 ref={panoRef}>Panoramy 360°</h2>
+                <div className="panos-gallery">
+                    {galleryItems.map(
+                        ({ title, url, cName, imgLink, type }) => {
+                            if (type === "360P") {
+                                return galleryRender(
+                                    imgLink,
+                                    cName,
+                                    title,
+                                    url
+                                );
+                            }
+                            return false;
+                        }
+                    )}
+                </div>
             </div>
-            <h2 ref={panoRef}>Panoramy 360°</h2>
-            <div className="panos-gallery">
-                {galleryItems.map(({ title, url, cName, imgLink, type }) => {
-                    if (type === "360P") {
-                        return galleryRender(imgLink, cName, title, url);
-                    }
-                    return false;
-                })}
-            </div>
-        </div>
-    );
+        );
+    }
 
     function galleryRender(imgLink, cName, title, url) {
         const image_link = `https://horizon17.pl/vt/galleryImages/${imgLink}`;
