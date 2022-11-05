809b2a592e6b54075beb06533ed2d2fa198842e6
Maciej Czapiewski <mcjczapiewski@gmail.com>
2022-11-05 19:34:42 +0100

    Change projects to new tab prices

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Gallery from "../components/Gallery";
-import Projects from "../components/Projects";
+/* import Projects from "../components/Projects"; */
 import Contact from "../components/Contact";
+import Prices from "../components/Prices";
 import About from "../components/About";
 import WhatIsIt from "../components/WhatIsIt";
 
 const Routes = ({ panoScroll, setPanoScroll, vtScroll, setVtScroll }) => (
     <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/aniapanato" component={Home} />
         <Route
             exact
             path="/gallery"
@@ -23,8 +24,9 @@ const Routes = ({ panoScroll, setPanoScroll, vtScroll, setVtScroll }) => (
                 />
             )}
         />
-        <Route path="/projects" component={Projects} />
+        {/* <Route path="/projects" component={Projects} /> */}
         <Route path="/contact" component={Contact} />
+        <Route path="/prices" component={Prices} />
         <Route path="/about" component={About} />
         <Route path="/whatisit" component={WhatIsIt} />
     </Switch>
